f5d11d2ac14b5d430ac3df2dd5d830f49b1ecebf
ielepassos <iele.passos@ccc.ufcg.edu.br>
2020-10-30 19:42:31 -0300

    mudanças feitas/hook

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,33 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Title from '../../components/Title'
 import Header from '../../components/Header'
 import './styles.css'
 
 const Login = () =>{ 
-  
-  
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")    
+    
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert(`Submitting Name ${name}`)
+        alert(`Submitting Password ${password}`)
+    }
+
     return(
-   <>
-    <Header></Header>
-    <div className = {'loginScreen'}>
-        <Title name ={"Login"}/>
-    </div>
-    <p>Preencha os campos abaixo para continuar</p>
-    <form >
-    <fieldset>
-        
-        <div class="input-block">
-            <label for="name">User</label>
-            <input name="name" id="name" required />
+    <>
+        <Header></Header>
+        <div className = {'loginScreen'}>
+            <Title name ={"Login"}/>
         </div>
-        <div class="input-block">
-            <label for="password">Senha</label>
-            <input name="password" id="password" type='password'required />
-        </div>
-        <button className ="Envios" type="submit">Login</button>
-    </fieldset>
-    </form>
-    </>
+        <p>Preencha os campos abaixo para continuar</p>
+        <form onSubmit={handleSubmit}>
+        <fieldset>
+            
+            <div class="input-block">
+                <label for="name">User</label>
+                <input name="name" id="name" required  onChange = {(e) => {setName(e.target.value)}}/>
+            </div>
+            <div class="input-block">
+                <label for="password">Senha</label>
+                <input name="password" id="password" type='password'required onChange = {(e) => {setPassword(e.target.value)}} />
+            </div>
+            <button className ="Envios" type="submit">Login</button>
+        </fieldset>
+        </form>
+        </>
     )
 }
 export default Login
